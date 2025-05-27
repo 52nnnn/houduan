acd30a7b37250441fc82767e3753a25547b1fef4
52nnnn <2764304248@qq.com>
2025-05-27 20:00:07 +0800

    后端

---
+++ src/main/java/org/example/User/userOper.java
@@ -0,0 +1,40 @@
+package org.example.User;
+
+public class userOper {
+    private String name;
+    private String password;
+    private String birthday;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
